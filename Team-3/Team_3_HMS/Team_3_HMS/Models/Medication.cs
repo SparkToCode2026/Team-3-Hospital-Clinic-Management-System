bbdd44291e806127d905800a0662e31244afcba9
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-08-04 13:34:26 +0400

    Implement Medication entity properties

--- Team-3/Team_3_HMS/Team_3_HMS/Models/Medication.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Models/Medication.cs
@@ -2,5 +2,14 @@
 {
     public class Medication
     {
+        public int MedicationID { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string GenericName { get; set; } = string.Empty;
+
+        public string DosageForm { get; set; } = string.Empty;
+
+        public decimal UnitPrice { get; set; }
     }
 }
