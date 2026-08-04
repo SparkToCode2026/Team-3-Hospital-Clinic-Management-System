a5820ac55f2d647cebe40f93dcb001d70be71c82
Omar Al-Balushi <om9763@gmail.com>
2026-08-04 15:27:42 +0400

    removed unnecessary files

--- Team-3/Team_3_HMS/Team_3_HMS/Models/PrescriptionItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Team_3_HMS.Models
-{
-    public class PrescriptionItem
-    {
-        public int PrescriptionID { get; set; }
-
-        public int MedicationID { get; set; }
-    }
-}
--- Team-3/Team_3_HMS/Team_3_HMS/ProjectContext.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/ProjectContext.cs
@@ -6,26 +6,25 @@ namespace Team_3_HMS
     {
         public class DBContext : DbContext
         {
-
-          
-            public DBContext(DbContextOptions<DBContext> options) : base(options)
-            {
-
-            }
             public DbSet<MedicalRecord> MedicalRecords { get; set; } = null!;
 
             public DbSet<Prescription> Prescriptions { get; set; } = null!;
 
             public DbSet<Medication> Medications { get; set; } = null!;
 
-            public DbSet<PrescriptionItem> PrescriptionItems { get; set; } = null!;
             public DbSet<DoctorProfile> DoctorProfiles { get; set; }
 
             public DbSet<Department> Departments { get; set; }
 
             public DbSet<Specialization> Specializations { get; set; }
 
-            public DbSet<DoctorSpecialization> DoctorSpecializations { get; set; }
+            public DBContext(DbContextOptions<DBContext> options) : base(options)
+            {
+
+            }
+            
+
+           
 
 
         }
