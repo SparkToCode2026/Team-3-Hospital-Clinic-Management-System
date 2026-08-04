32bd8a5f75544b201a6921c216e0b80d8c186b74
Omar Al-Balushi <om9763@gmail.com>
2026-08-04 13:23:48 +0400

    added user, patientprofile as well as invoices models and put them in dbcontext using dbset

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Models/PatientProfile.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Team_3_HMS.Models
+{
+    public class PatientProfile
+    {
+        [Key]
+        public int PatientProfileID { get; set; }
+
+        [Required]
+        public string gender { get; set; }
+
+        [Required]
+        public string DateOfBirth { get; set; }
+
+        [Required]
+        public string BloodGroup { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string emergencyContact { get; set; }
+
+        //owns
+
+        [ForeignKey("user")]
+        [Required]
+        public int userID { get; set; }
+
+        [JsonIgnore]
+        public user? user { get; set; }
+    }
+}
--- Team-3/Team_3_HMS/Team_3_HMS/ProjectContext.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/ProjectContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Team_3_HMS.Models;
 
 namespace Team_3_HMS
 {
@@ -6,8 +7,13 @@ namespace Team_3_HMS
     {
         public class DBContext : DbContext
         {
+            //registering models
 
-          
+            public DbSet<user> Users { get; set; }
+            public DbSet<PatientProfile> PatientProfiles { get; set; }
+            public DbSet<Invoice> Invoices { get; set; }
+
+
             public DBContext(DbContextOptions<DBContext> options) : base(options)
             {
 
