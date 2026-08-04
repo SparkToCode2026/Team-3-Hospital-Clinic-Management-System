5de6b9d4d753d69753dfb96cee853a3946d444ec
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-08-04 13:28:44 +0400

    Implement Prescription entity properties

--- Team-3/Team_3_HMS/Team_3_HMS/Models/Prescription.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Models/Prescription.cs
@@ -2,5 +2,14 @@
 {
     public class Prescription
     {
+        public int PrescriptionID { get; set; }
+
+        public DateTime IssuedDate { get; set; }
+
+        public string Instructions { get; set; } = string.Empty;
+
+        public string Notes { get; set; } = string.Empty;
+
+        public int MedicalRecordID { get; set; }
     }
 }
