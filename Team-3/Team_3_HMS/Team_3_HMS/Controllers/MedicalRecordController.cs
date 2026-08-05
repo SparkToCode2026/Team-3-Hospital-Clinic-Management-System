0eb116e4e420a81c7057d32ffa7b50816249b1dd
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-08-06 02:28:36 +0400

    Create MedicalRecord controller

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/MedicalRecordController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Team_3_HMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicalRecordController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public MedicalRecordController(ProjectContext context)
+        {
+            _context = context;
+        }
+    }
+}
