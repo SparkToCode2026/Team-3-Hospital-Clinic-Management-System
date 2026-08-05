1c27dfd393236a4cd135f98ab56cdc17b597f275
om3r2477-del <om3r2477@gmail.com>
2026-08-05 21:59:10 +0400

    (POST) to Create new Specialization

--- Team-3/Team_3_HMS/Team_3_HMS/Controllers/SpecializationController.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/SpecializationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Team_3_HMS.Models;
 
 namespace Team_3_HMS.Controllers
 {
@@ -11,6 +12,18 @@ namespace Team_3_HMS.Controllers
         public SpecializationController(ProjectContext _context)
         {
             context = _context;
+        }
+        // (POST) Create new Specialization
+        [HttpPost("AddSpecialization")]
+        public IActionResult AddSpecialization(Specialization specialization)
+        {
+            context.Specializations.Add(specialization);
+            context.SaveChanges();
+            return Ok(new
+            {
+                message = "Specialization added successfully",
+                specialization = specialization
+            });
         }
     }
 }
