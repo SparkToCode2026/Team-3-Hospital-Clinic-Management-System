bad220021a220902d27ae47e98f02ef127811ea1
om3r2477-del <om3r2477@gmail.com>
2026-08-05 21:55:53 +0400

    Created (Specialization controller) and connected it with ProjectContext

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/SpecializationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_3_HMS.Controllers
+{
+    [ApiController]
+    [Route("Specialization")]
+    public class SpecializationController : ControllerBase
+    {
+        private ProjectContext context;
+
+        public SpecializationController(ProjectContext _context)
+        {
+            context = _context;
+        }
+    }
+}
