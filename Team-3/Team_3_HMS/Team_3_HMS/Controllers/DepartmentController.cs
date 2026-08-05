309d964ed02b8e83626574b95dda0f43f16561b7
om3r2477-del <om3r2477@gmail.com>
2026-08-05 19:20:17 +0400

    Method : (PUT) to Update all Department details

--- Team-3/Team_3_HMS/Team_3_HMS/Controllers/DepartmentController.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/DepartmentController.cs
@@ -25,5 +25,26 @@ namespace Team_3_HMS.Controllers
                 department = department
             });
         }
+        // PUT to Update all Department details
+        [HttpPut("UpdateDepartment")]
+        public IActionResult UpdateDepartment(int id, Department updatedDepartment)
+        {
+            Department department = context.Departments
+            .FirstOrDefault(d => d.DepartmentId == id); //update by id 
+            if (department == null)
+            {
+                return NotFound("Department not found");
+            }
+            department.Name = updatedDepartment.Name;
+            department.Description = updatedDepartment.Description;
+            department.BuildingLocation = updatedDepartment.BuildingLocation;
+            department.DoctorProfileId = updatedDepartment.DoctorProfileId;
+            context.SaveChanges();
+            return Ok(new
+            {
+                message = "Department updated successfully",
+                updatedDepartment = department
+            });
+        }
     }
 }
