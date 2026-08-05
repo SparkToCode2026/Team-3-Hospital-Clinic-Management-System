de46787309529a532694d2d0f2a5a87a9526b381
om3r2477-del <om3r2477@gmail.com>
2026-08-05 13:46:36 +0400

    Method: (POST) to create new doctor Profile

--- Team-3/Team_3_HMS/Team_3_HMS/Controllers/DoctorProfileController.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/DoctorProfileController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Team_3_HMS.Models;
 
 namespace Team_3_HMS.Controllers
 {
@@ -11,6 +12,16 @@ namespace Team_3_HMS.Controllers
         public DoctorProfileController(ProjectContext context)
         {
             context = context;
+        }
+        // Method: POST create new doctor Profile
+        [HttpPost("AddDoctorProfile")]
+        public IActionResult AddDoctorProfile(DoctorProfile doctor)
+        {
+            context.DoctorProfiles.Add(doctor);
+
+            context.SaveChanges();
+            
+            return Ok(doctor.DoctorProfileId);
         }
     }
 }
