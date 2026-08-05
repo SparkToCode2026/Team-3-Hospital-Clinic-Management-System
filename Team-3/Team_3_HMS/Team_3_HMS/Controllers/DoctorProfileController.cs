06a492d03de4b3284373fd83a320849c5d5fd3bd
om3r2477-del <om3r2477@gmail.com>
2026-08-05 13:36:29 +0400

    create (DoctorProfileController) & Add the database context

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/DoctorProfileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_3_HMS.Controllers
+{
+    [ApiController]
+    [Route("DoctorProfile")]
+    public class DoctorProfileController : ControllerBase
+    {
+        private ProjectContext context;
+
+        public DoctorProfileController(ProjectContext context)
+        {
+            context = context;
+        }
+    }
+}
