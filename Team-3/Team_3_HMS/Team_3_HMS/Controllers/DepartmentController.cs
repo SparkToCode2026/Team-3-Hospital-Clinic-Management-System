f8ba5406817519d644dd50e5af7413ff50d48fa8
om3r2477-del <om3r2477@gmail.com>
2026-08-05 19:11:52 +0400

    Created (Department controller) and connected it with ProjectContext

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/DepartmentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_3_HMS.Controllers
+{
+    [ApiController]
+    [Route("Department")]
+    public class DepartmentController : ControllerBase
+    {
+        private ProjectContext context;
+
+        public DepartmentController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+    }
+}
