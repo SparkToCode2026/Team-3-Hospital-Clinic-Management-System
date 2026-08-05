2627452385ae60d4a30c3ad4702f35ff241fd932
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-08-06 03:02:41 +0400

    Add create endpoint for MedicalRecord

--- Team-3/Team_3_HMS/Team_3_HMS/Controllers/MedicalRecordController.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/MedicalRecordController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Team_3_HMS.Models;
+using Team_3_HMS;
 
 namespace Team_3_HMS.Controllers
 {
@@ -12,6 +16,27 @@ namespace Team_3_HMS.Controllers
         public MedicalRecordController(ProjectContext context)
         {
             _context = context;
+        }
+        // Case 1: 
+        [HttpPost]
+        [Authorize(Roles = "Doctor,Admin")]
+        public async Task<ActionResult<MedicalRecord>> CreateMedicalRecord(
+            MedicalRecord medicalRecord)
+        {
+            var appointmentExists = await _context.Appointments
+                .AnyAsync(a => a.AppointmentId == medicalRecord.AppointmentId);
+
+            if (!appointmentExists)
+            {
+                return BadRequest("Appointment not found.");
+            }
+
+            _context.MedicalRecords.Add(medicalRecord);
+            await _context.SaveChangesAsync();
+
+            return Created(
+                $"/api/MedicalRecord/{medicalRecord.MedicalRecordID}",
+                medicalRecord);
         }
     }
 }
