2c1a7b01233d7c13326b0b86b9547947a52b46ed
alaaibrahim24 <alaaibrahimal@icloud.com>
2026-08-06 23:47:00 +0400

    add project file

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/PrescriptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_3_HMS.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
