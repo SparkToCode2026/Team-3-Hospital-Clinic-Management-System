f8a7657934c2dd616b149edffb6ac0de8671edb4
Omar Al-Balushi <om9763@gmail.com>
2026-08-06 12:09:42 +0400

    patientcontroller create and prepared

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/PatientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Team_3_HMS.Models;
+
+namespace Team_3_HMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientProfileController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public PatientProfileController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
