3691bf56eb23d404f63bce326ea81cc32b9699a1
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-08-06 18:44:28 +0400

    Remove unused using

--- Team-3/Team_3_HMS/Team_3_HMS/Controllers/LabTestController.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/LabTestController.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
+
 using Team_3_HMS.Models;
 
 namespace Team_3_HMS.Controllers
