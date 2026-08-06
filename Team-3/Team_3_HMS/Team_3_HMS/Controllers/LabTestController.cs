35d1df396f0bc947075d262704538e0b595b5d55
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-08-06 17:18:57 +0400

    Create LabTest controller

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/LabTestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Team_3_HMS.Models;
+
+namespace Team_3_HMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabTestController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public LabTestController(ProjectContext context)
+        {
+            _context = context;
+        }
+    }
+}
