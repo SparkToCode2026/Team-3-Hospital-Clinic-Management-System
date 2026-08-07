524190861f66192aa43dd0fc3346837d323962df
alaaibrahim24 <alaaibrahimal@icloud.com>
2026-08-07 15:14:52 +0400

    Create MedicationController structure

---
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/MedicationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Team_3_HMS.Models;
+
+namespace Team_3_HMS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class MedicationController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public MedicationController(ProjectContext context)
+        {
+            _context = context;
+        }
+    }
+}
