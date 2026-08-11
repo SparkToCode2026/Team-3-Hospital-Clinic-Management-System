f3aaff64775f47addcd638ed22d1673ba4514085
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-08-12 01:08:18 +0400

    Edit some of codes

--- Team-3/Team_3_HMS/Team_3_HMS/Controllers/PrescriptionController.cs
+++ Team-3/Team_3_HMS/Team_3_HMS/Controllers/PrescriptionController.cs
@@ -41,7 +41,7 @@ namespace Team_3_HMS.Controllers
                     await _emailService.SendEmailAsync(
                         patient.email,
                         "Prescription Created",
-                        $"Your prescription has been created.\n\nInstructions: {prescription.Instructions}"
+                        OMR"Your prescription has been created.\n\nInstructions: {prescription.Instructions}"
                     );
                 }
             }
